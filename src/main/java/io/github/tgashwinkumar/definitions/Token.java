1ca1cd5d8567875af95b10979355e50ace601022
tgashwinkumar <tgashwinkumar@gmail.com>
2021-07-29 20:42:33 +0530

    Parser: introduced Interfaces

--- src/main/java/io/github/tgashwinkumar/definitions/Token.java
+++ src/main/java/io/github/tgashwinkumar/definitions/Token.java
@@ -1,9 +1,13 @@
 package io.github.tgashwinkumar.definitions;
 
-public class Token {
+public class Token implements NodeType{
 
     public TokenType tokenType;
     public String tokenValue;
+
+    public RETURN_TYPE typeOf() {
+        return RETURN_TYPE.TOKEN;
+    }
 
     public Token(TokenType ttype, String tvalue){
         this.tokenType = ttype;
@@ -13,5 +17,14 @@ public class Token {
     @Override
     public String toString() {
         return this.tokenType.name() + "-Token@\"" + this.tokenValue + "\"";
+    }
+
+    public int getPrecedence(){
+        if(this.tokenType == TokenType.AND) {
+            return 2;
+        } else if (this.tokenType == TokenType.OR){
+            return 1;
+        }
+        return -1;
     }
 }
--- src/main/java/io/github/tgashwinkumar/parser/Parser.java
+++ src/main/java/io/github/tgashwinkumar/parser/Parser.java
@@ -1,38 +1,60 @@
 package io.github.tgashwinkumar.parser;
 
 import io.github.tgashwinkumar.definitions.InputArray;
+import io.github.tgashwinkumar.definitions.NodeType;
+import io.github.tgashwinkumar.definitions.Position;
 import io.github.tgashwinkumar.definitions.Token;
+import io.github.tgashwinkumar.definitions.TokenType;
 
 public class Parser {
     
     private Token[] tokenList;
     private InputArray inputArray;
-    private Object[] operandStack;
-    private Object[] operatorStack;
-
+    private NodeType[] operandStack;
+    private Token[] operatorStack;
+    private Token currentToken;
+    private Position currentPos = new Position(-1);
 
     public Parser(InputArray inputArr, Token[] tokens){
         this.inputArray = inputArr;
         this.tokenList = tokens;
     }
 
-    private void printStack(Object[] stack){
+    private void printStack(NodeType[] stack){
         if(stack == null){
             System.out.printf("No element");
         }else{
-            for(Object e: stack){
+            for(NodeType e: stack){
                 System.out.printf("%d, ", e);
             }
             System.out.println("");
         }
     }
 
-    private Object pushToOperandStack(Object newToken){
+    private NodeType getCurrentFromOperandStack(){
+        int len = 0;
+        if (this.operandStack != null) {
+            len = this.operandStack.length;
+            return this.operandStack[len-1];
+        }
+        return null;
+    }
+
+    private Token getCurrentFromOperatorStack() {
+        int len = 0;
+        if (this.operatorStack != null) {
+            len = this.operatorStack.length;
+            return this.operatorStack[len - 1];
+        }
+        return null;
+    }
+
+    private NodeType pushToOperandStack(NodeType newToken){
         int len = 0;
         if (this.operandStack != null) {
             len = this.operandStack.length;
         }
-        Object[] newStack = new Object[len+1];
+        NodeType[] newStack = new NodeType[len+1];
         for(int i = 0; i < len; i++){
             newStack[i] = this.operandStack[i];
         } 
@@ -41,26 +63,26 @@ public class Parser {
         return newToken;
     }
 
-    private Object popFromOperandStack(){
+    private NodeType popFromOperandStack(){
         int len = 0;
         if (this.operandStack != null) {
             len = this.operandStack.length;
         }
-        Object[] newStack = new Object[len - 1];
+        NodeType[] newStack = new NodeType[len - 1];
         for (int i = 0; i < len - 1; i++) {
             newStack[i] = this.operandStack[i];
         }
-        Object popElement = this.operandStack[len-1];
+        NodeType popElement = this.operandStack[len-1];
         this.operandStack = newStack;
         return popElement;
     }
 
-    private Object pushToOperatorStack(Object newToken) {
+    private Token pushToOperatorStack(Token newToken) {
         int len = 0;
         if (this.operatorStack != null) {
             len = this.operatorStack.length;
         }
-        Object[] newStack = new Object[len + 1];
+        Token[] newStack = new Token[len + 1];
         for (int i = 0; i < len; i++) {
             newStack[i] = this.operatorStack[i];
         }
@@ -69,21 +91,43 @@ public class Parser {
         return newToken;
     }
 
-    private Object popFromOperatorStack() {
+    private Token popFromOperatorStack() {
         int len = 0;
         if (this.operatorStack != null) {
             len = this.operatorStack.length;
         }
-        Object[] newStack = new Object[len - 1];
+        Token[] newStack = new Token[len - 1];
         for (int i = 0; i < len - 1; i++) {
             newStack[i] = this.operatorStack[i];
         }
-        Object popElement = this.operatorStack[len - 1];
+        Token popElement = this.operatorStack[len - 1];
         this.operatorStack = newStack;
         return popElement;
     }
 
+    private void nextToken(){
+        this.currentPos.nextPosition();
+        if(this.currentPos.position < this.tokenList.length){
+            this.currentToken = this.tokenList[this.currentPos.position];
+        }else{
+            this.currentToken = null;
+        }
+    }
+
     public void runParser(){
-        
+        this.nextToken();
+        while(this.currentToken != null){
+            if(this.currentToken.tokenType == TokenType.INT){
+                this.pushToOperandStack(this.currentToken);
+                this.nextToken();
+            } else if (this.currentToken.tokenType == TokenType.SOP || this.currentToken.tokenType == TokenType.POS){
+                this.pushToOperatorStack(this.currentToken);
+                this.nextToken();
+            } else if (this.currentToken.getPrecedence() > 0) {
+                while (this.getCurrentFromOperatorStack().tokenType != TokenType.LPAREN) {
+                    
+                }
+            }
+        }
     }
 }
