a224be82b9180c93682546a02ef4fdbce2aaa7f5
tgashwinkumar <tgashwinkumar@gmail.com>
2021-07-29 20:42:52 +0530

    Parser: introduced Interfaces

---
+++ src/main/java/io/github/tgashwinkumar/errors/InvalidDataType.java
@@ -0,0 +1,7 @@
+package io.github.tgashwinkumar.errors;
+
+public class InvalidDataType extends Exception {
+    public InvalidDataType(){
+        super("INVALID DATA TYPE: Do check the types mentioned.");
+    }
+}
